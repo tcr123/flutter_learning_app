525d495e0217502f8f1b8c4aa8b7be1398c16b7b
ChunRong <chunrong1805@gmail.com>
2021-12-06 11:06:48 +0800

    nineth commit

---
+++ lib/models/enrolled_model.dart
@@ -0,0 +1,11 @@
+class EnrolledModel {
+  final String class_name;
+  final String subject;
+  final String lecture_name;
+
+  EnrolledModel({
+    required this.class_name,
+    required this.subject,
+    required this.lecture_name,
+  });
+}
